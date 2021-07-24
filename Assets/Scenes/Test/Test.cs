fcf123770a2945e65e487bc043f7fe4b830b53c2
Panit <panit.cuteza@mail.kmutt.ac.th>
2021-07-24 10:12:31 +0700

    Initial commit

---
+++ Assets/Scenes/Test/Test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Test");
+        Debug.Log("Kuy");
+        Debug.Log("Hee");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# NSC2021
+ NSC in 2021 whit my friends
